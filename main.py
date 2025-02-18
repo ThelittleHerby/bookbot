0d2b09cc0626e68076b9c27d906b7bae1c555a68
ThelittleHerby <herbert.marcel@gmx.de>
2025-02-18 12:33:10 +0100

    Bookbot in Python geschrieben

---
+++ main.py
@@ -0,0 +1,33 @@
+def count_word(text):
+    return len(text.split())
+
+def count_characters(text):
+    char_count = {}
+    for char in text.lower():
+        if char.isalpha():
+            char_count[char] = char_count.get(char, 0) + 1
+    return char_count
+
+def main():
+    try:
+        file_path = "books/frankenstein.txt"
+        with open(file_path, "r", encoding="utf-8") as f:
+            file_contents = f.read()
+        
+        word_count = count_word(file_contents)
+        char_count = count_characters(file_contents)
+        
+        print(f"--- Begin report of {file_path} ---")
+        print(f"{word_count} words found in the document\n")
+        
+        for char, count in sorted(char_count.items()):
+            print(f"The '{char}' character was found {count} times")
+        
+        print("--- End report ---")
+    except FileNotFoundError:
+        print("Die Datei wurde nicht gefunden.")
+    except Exception as e:
+        print(f"Ein Fehler ist aufgetreten: {e}")
+
+if __name__ == "__main__":
+    main()
